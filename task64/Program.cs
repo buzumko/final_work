b5a55ebaf8ccfad6bbfd2173c3ab5c57fb3533ae
buzum <buzum@mail.ru>
2023-03-26 22:31:16 +0400

    task 64 for Final Work

---
+++ task64/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+//числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+int UserNumber = GetNumberFromUser("Задайте целое чисел N: ","Ошибка ввода!");
+Console.Write($"N = {UserNumber} -> ");
+PrintInteger(UserNumber);
+
+///////
+
+// Выводит в консоль сообщение message,
+// преобразовывает введённую пользователем строку в число типа int.
+// В случае ошибки выводит в консоль сообщение errorMessage
+int GetNumberFromUser(string message, string errorMessage)
+{ 
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect) return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+
+// рекурсия
+void PrintInteger(int n)
+{
+    Console.Write($"{n}, ");
+    if(n == 1) goto exit;
+    else PrintInteger(n-1);
+    exit:;
+}
