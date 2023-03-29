7e2240a6d62ac55e120897ba5ef5d981dc00cf80
buzum <buzum@mail.ru>
2023-03-29 17:27:33 +0400

    task 68 for Final Work

---
+++ task68/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+uint UserNumberN = GetNumberFromUser("Задайте целое неотрицательное n: ","Ошибка ввода!");
+uint UserNumberM = GetNumberFromUser("Задайте целое неотрицательное m: ","Ошибка ввода!");
+Console.Write($"N = {UserNumberN}, M = {UserNumberM}-> A({UserNumberN}, {UserNumberM}) = ");
+uint Akkerman = 0;
+Akkerman = GetAkkerman(UserNumberN, UserNumberM);
+
+Console.WriteLine(Akkerman);
+
+///////
+
+// Выводит в консоль сообщение message,
+// преобразовывает введённую пользователем строку в число типа uint.
+// В случае ошибки выводит в консоль сообщение errorMessage
+uint GetNumberFromUser(string message, string errorMessage)
+{ 
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = uint.TryParse(Console.ReadLine(), out uint userNumber);
+        if(isCorrect) return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+
+/// Функция Аккермана
+uint GetAkkerman(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return GetAkkerman(n - 1, 1);
+    else
+      return GetAkkerman(n - 1, GetAkkerman(n, m - 1));
+}
